9a75ff17b1d14a4a28af9006abba94ae1e14a3c9
PhanTai22 <125341341+PhanTai22@users.noreply.github.com>
2023-02-14 16:09:05 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Phương Thùy';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100059807335395';
+const eventName = 'Valentine';
+const titleCard = 'Tặng em';
+const contentCard = 'Gửi Thùy!!!💘My little angel, dù khoảng cách bắc nam có xa đến mấy thì cũng không ngăn cản được tấm lòng của anh. Chúc em một ngày Valentine vui vẻ và dịu dàng hơn với anh nhé <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
